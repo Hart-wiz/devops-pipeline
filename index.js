5551f63b3caa7cacca6413acd8cac8196173ba39
wisdom njimogu <njimoguwisdom@gmail.com>
2026-04-03 15:02:30 +0100

    added ci pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello,  DevoPs!");
+console.log("I am learning DevOps and CI/CD pipelines.");
---
+++ test.js
@@ -0,0 +1,7 @@
+console.log("Starting test ...");
+console.log("This is a test file to verify the CI/CD pipeline.");
+console.log("If you see this message, the pipeline is working correctly.");
+
+setTimeout(() => {
+  console.log("waiting 3 seconds before finishing the test...");
+}, 2000);
