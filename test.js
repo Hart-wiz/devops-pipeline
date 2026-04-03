34fb75b1e508411de090757a52adfee9b50ddcab
wisdom njimogu <njimoguwisdom@gmail.com>
2026-04-03 15:19:54 +0100

    add jest for test

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 console.log("Starting test ...");
 console.log("This is a test file to verify the CI/CD pipeline.");
 console.log("If you see this message, the pipeline is working correctly.");
-
+// sum.test.js
+test("adds 1 + 2", () => {
+  expect(1 + 2).toBe(3);
+});
 setTimeout(() => {
   console.log("waiting 3 seconds before finishing the test...");
 }, 2000);
